1c9a3b927ce7c5d945ab68b2407964374265f608
Neil Sullivan <ac30delay@gmail.com>
2023-04-12 15:53:06 -0500

    initial commit

---
+++ Repositories/IVideoRepository.cs
@@ -0,0 +1,20 @@
+﻿using Streamish.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+        void Add(Video video);
+        void Delete(int id);
+        List<Video> GetAll();
+        Video GetById(int id);
+        void Update(Video video);
+        List<Video> GetAllWithComments();
+        Video GetVideoByIdWithComments(int id);
+        List<Video> Search(string criterion, bool sortDescending);
+        List<Video> SearchTitle(string title);
+        List<Video> SearchByDate(DateTime title);
+    }
+}
